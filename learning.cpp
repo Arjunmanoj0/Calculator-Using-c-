80e4ecbcac127b06e71aa36f81921cf480440918
Arjun Manoj <arjunmaniyoor@gmail.com>
2024-01-18 18:49:58 +0530

    first commit

--- learning.cpp
+++ learning.cpp
@@ -1,37 +1,49 @@
-#include<stdio.h>
 #include<iostream>
 using namespace std;
-
 int main()
 {
-    int a,b,c,d;
-    
-    cout<<"enter the 1st no:";
-    cin>>a;
-
-    cout<<"enter the 2nd no:";
-    cin>>b;
-
-    cout<<"1.add\n";
-    cout<<"2.substract\n";
-    cin>>c;
-
-    switch (c)
-    {
-    case 1:
-        d=a+b;
-        cout<<d;
+	int amount;
+	int n1,n2,n5,n10,n20,n50,n100,n500;
+	n1 = n2 = n5 = n10 = n20 = n50 = n100 = n500 =0;
+	cout<<"Please Enter Your total Amount to find the notes : ";
+	cin>>amount;
+	switch(amount>=500)
+	{
+		case 1:
+			n500 = amount/500;
+        	amount -= n500 * 500;
         break;
-        case 2:
-        d=a-b;
-        cout<<d;
+	}
+	switch(amount >=100)
+	{
+		case 1:
+			n100 = amount/100;
+        	amount -= n100 * 100;
         break;
-    
-    default:
+	}
+	switch(amount >=50)
+	{
+		case 1:
+			n50 = amount/50;
+        	amount -= n50 * 50;
+	}
+	switch(amount >=20)
+	{
+		case 1:
+			n20 = amount/20;
+        	amount -= n20 * 20;
         break;
-    }
-    
-    
-
-    return 0;
+	}
+	switch(amount >=10)
+	{
+		case 1:
+			n10 = amount/10;
+        	amount -= n10 * 10;
+        break;
+	}
+	cout << "500 = " << n500 <<endl;
+    cout << "100 = " << n100 <<endl;
+    cout << "50 = " << n50 <<endl;
+    cout << "20 = " << n20 <<endl;
+    cout << "10 = " << n10 <<endl;
 }
